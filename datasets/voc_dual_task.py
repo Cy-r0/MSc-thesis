30a47dcf13bea5d37a342212accb4e9209b27ed1
Cy-r0 <cirocursio@live.it>
2019-08-14 17:23:13 +0100

    updated dataset files on remote repo

--- datasets/voc_dual_task.py
+++ datasets/voc_dual_task.py
@@ -7,10 +7,10 @@ from torch.utils.data import Dataset
 import torchvision.transforms as T
 
 
-class VOCDistance(Dataset):
+class VOCDualTask(Dataset):
     """
     Dataset based on PASCAL VOC 2012 (assumes it has already been downloaded).
-    It uses rgb as samples and instance distance transform as labels.
+    It uses rgb as samples and (instance segmentation, distance transform) as labels.
 
     Arguments:
         - root (string): root directory of dataset (devkit).
@@ -27,7 +27,8 @@ class VOCDistance(Dataset):
         self.transform = transform
         voc_root = os.path.join(root, "VOCdevkit/VOC2012")
         image_dir = os.path.join(voc_root, "JPEGImages")
-        target_dir = os.path.join(voc_root, "DistanceTransform")
+        seg_dir = os.path.join(voc_root, "SegmentationClassAug")
+        dist_dir = os.path.join(voc_root, "DistanceTransform")
 
         if not os.path.isdir(voc_root):
             raise RuntimeError("Dataset not found or corrupted.")
@@ -36,16 +37,17 @@ class VOCDistance(Dataset):
         imageset_file = os.path.join(imageset_dir, image_set.rstrip('\n') + ".txt")
 
         if not os.path.exists(imageset_file):
-            raise ValueError(
-                "Wrong image_set entered!")
+            raise ValueError("Wrong image_set entered!")
 
         with open(os.path.join(imageset_file), "r") as f:
             file_names = [x.strip() for x in f.readlines()]
 
         self.images = [os.path.join(image_dir, x + ".jpg") for x in file_names]
-        self.targets = [os.path.join(target_dir, x + ".png") for x in file_names]
+        self.segs = [os.path.join(seg_dir, x + ".png") for x in file_names]
+        self.dists = [os.path.join(dist_dir, x + ".png") for x in file_names]
         
-        assert len(self.images) == len(self.targets)
+        assert (len(self.images) == len(self.segs)
+            and len(self.images) == len(self.dists))
     
     def __getitem__(self, index):
         """
@@ -56,8 +58,10 @@ class VOCDistance(Dataset):
             - tuple: image (PIL image), target (PIL Image).
         """
         img = Image.open(self.images[index])
-        target = Image.open(self.targets[index])
-        sample = {'image': img, 'target': target}
+        seg = Image.open(self.segs[index])
+        dist = Image.open(self.dists[index])
+
+        sample = {"image": img, "seg": seg, "dist": dist}
 
         if self.transform is not None:
             sample = self.transform(sample)
